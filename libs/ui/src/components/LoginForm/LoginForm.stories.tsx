4f244c92c9cdca8b64271f6a8954b586d028f59c
MustafaTRKYLMZ <cana66331@gmail.com>
2022-07-21 15:55:33 +0200

    feat/login form added

---
+++ libs/ui/src/components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,44 @@
+import { Textarea } from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Story, Meta } from '@storybook/react'
+import { useForm } from 'react-hook-form'
+import { FaEnvelope } from 'react-icons/fa'
+import * as yup from 'yup'
+
+import { LoginForm, LoginFormProps } from '.'
+
+export default {
+  title: 'Forms/LoginForm',
+  component: LoginForm,
+} as Meta<typeof LoginForm>
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+})
+
+const Template: Story<LoginFormProps> = args => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'all',
+  })
+
+  return <LoginForm {...args} name="email" errors={errors} register={register} />
+}
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const LeftElement = Template.bind({})
+LeftElement.args = {
+  leftElement: <FaEnvelope />,
+}
+
+export const AsTextarea = Template.bind({})
+AsTextarea.args = {
+  label: 'Textarea',
+  placeholder: 'Enter text',
+  as: Textarea,
+}
---
+++ libs/ui/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,162 @@
+/* eslint-disable import/order */
+import {
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  Heading,
+  HStack,
+  InputProps,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import * as React from 'react'
+import { useState } from 'react'
+import {
+  FieldErrors,
+  FieldValues,
+  useForm,
+  SubmitHandler,
+  UseFormRegister,
+} from 'react-hook-form'
+import * as yup from 'yup'
+
+import { OAuthButtonGroup } from './oauth-button-group'
+
+import { Navigate } from '~components'
+import { useAuth } from '~hooks'
+import { FormItem } from './form-item'
+
+const schema = t =>
+  yup.object({
+    password: yup
+      .string()
+      .min(8, t('login.password.warning'))
+      .required(t('login.password.required'))
+      .matches(RegExp('(.*[a-z].*)'), t('login.password.matches.lowercase'))
+      .matches(RegExp('(.*[A-Z].*)'), t('login.password.matches.uppercase'))
+      .matches(RegExp('(.*\\d.*)'), t('login.password.matches.number'))
+      .matches(
+        RegExp('[!@#$%^&*(),.?":{}|<>]'),
+        t('login.password.matches.special'),
+      ),
+    email: yup
+      .string()
+      .email(t`contact.form.email-invalid`)
+      .required(t`login.email.required`),
+  })
+
+export type LoginFormProps = InputProps & {
+  name: string
+  label?: string
+  placeholder?: string
+  helperText?: string
+  leftElement?: React.ReactNode
+  hideLabel?: boolean
+  errors: FieldErrors<FieldValues>
+  register: UseFormRegister<FieldValues>
+  onSubmitHandler: (data: FieldValues) => void
+}
+
+export const LoginForm: React.FC<LoginFormProps> = ({ onSubmitHandler }) => {
+  useAuth('/profile', true)
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  const { t } = useTranslation()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema(t)),
+    mode: 'all',
+  })
+
+  const handleSubmitSign: SubmitHandler<FieldValues> = async data => {
+    onSubmitHandler(data)
+  }
+
+  return (
+    <Container
+      maxW="lg"
+      py={{ base: '12', md: '24' }}
+      px={{ base: '0', sm: '8' }}
+    >
+      <Stack
+        spacing="8"
+        shadow="lg"
+        bg="white"
+        p={{ base: 8, lg: 12 }}
+        rounded="lg"
+      >
+        <Stack spacing="6">
+          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+            <Heading>{t('login.sign-in-header.title')}</Heading>
+            <HStack spacing="1" justify="center">
+              <Text color="muted">{t('login.sign-in-header.text')}</Text>
+              <Navigate
+                variant="link"
+                as={Button}
+                href="/user/register"
+                colorScheme="blue"
+              >
+                {t('login.sign-in-header.button')}
+              </Navigate>
+            </HStack>
+          </Stack>
+        </Stack>
+        <Stack spacing="6" as="form" onSubmit={handleSubmit(handleSubmitSign)}>
+          <Stack spacing="5">
+            {errorMessage && <Text color="red.500">{errorMessage}</Text>}
+            <FormItem
+              id="email"
+              label={t('login.email.title')}
+              type="email"
+              register={register}
+              errors={errors}
+            />
+            <FormItem
+              id="password"
+              type="password"
+              label={t('login.password.title')}
+              autoComplete="current-password"
+              register={register}
+              errors={errors}
+            />
+          </Stack>
+          <HStack justify="space-between">
+            {/* TODO Set session exp time */}
+            <Checkbox defaultChecked>{t('login.remember-me')}</Checkbox>
+            <Navigate
+              as={Button}
+              href="/user/forgot-password"
+              variant="link"
+              colorScheme="blue"
+              size="sm"
+            >
+              {t('login.password.forgot-password')}
+            </Navigate>
+          </HStack>
+          <Stack spacing="6">
+            <Button type="submit" colorScheme="blue">
+              {t('login.sign-in')}
+            </Button>
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                {t('login.sign-in-with')}
+              </Text>
+              <Divider />
+            </HStack>
+            <OAuthButtonGroup />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
+  )
+}
