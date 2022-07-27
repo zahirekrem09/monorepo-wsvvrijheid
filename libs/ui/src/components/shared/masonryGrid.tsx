5e653de33807f92d27905dc7966d75f06dc7666e
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2022-07-27 12:59:03 +0200

    masonry-grid component converted

---
+++ libs/ui/src/components/shared/masonryGrid.tsx
@@ -0,0 +1,40 @@
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { FC, ReactElement } from 'react';
+import Masonry from 'react-masonry-css';
+
+interface MasonryGridProps {
+  gap: number;
+  cols: Array<number>;
+  children: ReactElement;
+}
+
+export const MasonryGrid: FC<MasonryGridProps> = ({
+  children,
+  gap = 4,
+  cols = [1, 2, 3, 4],
+}) => {
+  const breakpointCols = useBreakpointValue(cols);
+  return (
+    <Flex
+      as={Masonry}
+      ml={-gap}
+      breakpointCols={breakpointCols}
+      className="masonry-grid"
+      columnClassName="masonry-grid_column"
+      sx={{
+        '& .masonry-grid_column': {
+          '&:not(:nth-of-type(1))': {
+            bgClip: 'padding-box',
+            pl: { base: cols[0] === 1 ? 0 : gap, lg: gap },
+          },
+
+          '& > div': {
+            mb: gap,
+          },
+        },
+      }}
+    >
+      {children}
+    </Flex>
+  );
+};
