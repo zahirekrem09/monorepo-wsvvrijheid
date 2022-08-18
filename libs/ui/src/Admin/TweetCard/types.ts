2162a9a26b5576ed8c0ac900ea8fb2e47ec84bba
7alip <talipaltas@gmail.com>
2022-08-18 05:55:42 +0200

    Remove admin folder

--- libs/ui/src/Admin/TweetCard/types.ts
+++
@@ -1,8 +0,0 @@
-import { StackProps } from '@chakra-ui/react'
-import { Tweet } from '@wsvvrijheid/types'
-
-export type TweetBaseCardProps = {
-  tweet: Tweet
-  onSave: (tweet: Tweet) => void
-  onEdit: (tweet: Tweet) => void
-} & StackProps
--- libs/ui/src/admin/NewsCard/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from './NewsCardBase'
-export * from './types'
-export * from './ActionButton'
