98dcfb74f8714f86de0b015752a2dfec632061a0
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2022-08-23 16:00:50 +0200

    i made some change in request.ts otherwise did not able to solve some issues

---
+++ libs/utils/src/services/art/find.ts
@@ -0,0 +1,93 @@
+import { StrapiLocale } from '@wsvvrijheid/types'
+import qs from 'qs'
+import { useQuery } from 'react-query'
+
+import { request } from '../../lib'
+
+type GetArts = {
+  categories: string
+  populate: string | string[]
+  page: number
+  pageSize: number
+  searchTerm: string
+  username: string
+  sort: string | string[]
+  locale: StrapiLocale
+  token?: string
+}
+
+export const getArts = async ({
+  categories,
+  populate = ['artist.user.avatar', 'categories', 'images', 'likers'],
+  page = 1,
+  pageSize,
+  searchTerm,
+  username,
+  sort,
+  locale,
+  token,
+}: GetArts) => {
+  const userFilter = {
+    artist: {
+      user: {
+        username: {
+          $containsi: searchTerm || username,
+        },
+      },
+    },
+  }
+
+  const titleFilter = {
+    title: {
+      $containsi: searchTerm,
+    },
+  }
+
+  const statusFilter = {
+    status: {
+      $eq: 'approved',
+    },
+  }
+
+  const searchFilter = username
+    ? userFilter
+    : searchTerm && {
+        $or: [userFilter, titleFilter],
+      }
+
+  const categoryObj = qs.parse(categories)
+
+  const filters: { [key: string]: unknown } = {
+    ...(searchFilter || {}),
+    categories: { code: { $in: Object.values(categoryObj) } },
+    ...(statusFilter || {}),
+  }
+  return request({
+    url: 'api/arts',
+    filters,
+    page,
+    pageSize,
+    sort: sort || ['publishedAt:desc'],
+    locale,
+    populate,
+    token,
+  })
+}
+
+export const useArts = (
+  queryKey: string,
+  args: {
+    categories: string
+    populate: Array<string>
+    page: number
+    pageSize: number
+    searchTerm: string
+    username: string
+    sort: Array<string>
+    locale: StrapiLocale
+  },
+) =>
+  useQuery({
+    queryKey,
+    queryFn: () => getArts(args),
+  })
