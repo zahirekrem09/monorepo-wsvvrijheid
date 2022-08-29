6252fcc8d730308ba6897d23bc98cd0e415a4761
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-08-29 17:39:24 +0200

    volunteers page is created.

---
+++ apps/foundation/pages/volunteers.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react'
+
+import { VolunteersTemplate, VolunteersTemplateProps } from '@wsvvrijheid/ui'
+import { request } from '@wsvvrijheid/utils'
+import { getVolunteers } from '@wsvvrijheid/utils'
+import { GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { NextSeoProps } from 'next-seo'
+
+import { Layout } from '../components'
+import i18nConfig from '../next-i18next.config'
+
+const VolunteersPage: FC<VolunteersTemplateProps & { seo: NextSeoProps }> = ({
+  seo,
+  volunteers,
+  jobs,
+}) => {
+  return (
+    <Layout seo={seo} isDark>
+      <VolunteersTemplate seo={seo} volunteers={volunteers} jobs={jobs} />
+    </Layout>
+  )
+}
+export default VolunteersPage
+
+export const getStaticProps: GetStaticProps = async context => {
+  const volunteersResponse = await getVolunteers()
+  const jobsResponse = await request()({ url: 'api/jobs' })
+
+  const title = {
+    en: 'Volunteers',
+    nl: 'Vrijwillegers',
+    tr: 'Gönüllüler',
+  }
+
+  const seo = {
+    title: title[context.locale],
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(context.locale, ['common'], i18nConfig)),
+      volunteers: volunteersResponse,
+      jobs: jobsResponse?.data,
+      seo,
+    },
+    revalidate: 1,
+  }
+}
