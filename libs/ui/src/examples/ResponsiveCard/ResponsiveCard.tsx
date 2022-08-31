0645f699c9a932ce0d548ff3552468baaa265e4f
selman <gunduzgece546@gmail.com>
2022-08-31 19:28:37 +0300

    image width and height changed

--- libs/ui/src/examples/ResponsiveCard/ResponsiveCard.tsx
+++ libs/ui/src/examples/ResponsiveCard/ResponsiveCard.tsx
@@ -16,14 +16,14 @@ export const ResponsiveCard = () => {
       <Stack>
         <Image
           w={['300px', null, 'full']}
-          h={['300px', null]}
+          h={['300px', null, 'full']}
           src={randomPic}
           borderRadius={['full', null, 'none']}
           m="auto"
         />
       </Stack>
 
-      <Stack spacing={2} w={['full', null, '80%']}>
+      <Stack spacing={2} w={['full', null]}>
         <Heading as="h1" size="lg" mb={5} color="#000" fontWeight="500">
           This is Heading...
         </Heading>
