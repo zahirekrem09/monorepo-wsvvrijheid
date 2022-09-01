3ecef69165a7a59ee2d36d7150791518583a82f4
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-09-01 17:06:23 +0200

    register service

---
+++ apps/admin/pages/api/auth/register.ts
@@ -0,0 +1,48 @@
+import { Session } from '@wsvvrijheid/types'
+import { mutation, sessionOptions, getAuth } from '@wsvvrijheid/utils'
+import axios from 'axios'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { NextApiResponse, NextApiRequest } from 'next'
+
+const registerRoute = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, username, email, password } = req.body
+
+  const trimmedName = name.trim()
+  const trimmedUsername = username.trim()
+  const trimmedEmail = email.trim()
+
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/auth/local/register`,
+      {
+        username: trimmedUsername,
+        email: trimmedEmail,
+        password,
+      },
+    )
+
+    const token = response.data.jwt
+    const userId = response.data.user?.id
+
+    await mutation(token).post('api/artists', {
+      data: { user: userId, name: trimmedName },
+    })
+
+    const auth = await getAuth(email, password)
+
+    req.session = auth as Session
+
+    await req.session.save()
+    res.json(auth)
+  } catch (error) {
+    if (!error.response?.data?.error.message) {
+      return res.status(500).json({ message: 'Internal server error' })
+    } else {
+      res.json(error.response?.data)
+    }
+  }
+}
+
+const handler = withIronSessionApiRoute(registerRoute, sessionOptions)
+
+export default handler
