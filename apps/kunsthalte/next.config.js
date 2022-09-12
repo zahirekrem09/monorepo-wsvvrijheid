b5e7666ae5f1b31bfe9ee98e6dda0918dae6c9a4
7alip <talipaltas@gmail.com>
2022-09-13 01:18:13 +0200

    Add kunsthalte project

---
+++ apps/kunsthalte/next.config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNx = require('@nrwl/next/plugins/with-nx')
+
+const { i18n } = require('./next-i18next.config')
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  i18n,
+  images: {
+    domains: ['api.samenvvv.nl', 'wsvvrijheid.nl', 'localhost'],
+  },
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+}
+
+module.exports = withNx(nextConfig)
