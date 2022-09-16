5a11f997d14fad4241f2794dbece2e8b74396163
Akin <akin.tanis.dev@gmail.com>
2022-09-16 17:26:38 +0200

    CreateArtForm story refactor

--- libs/ui/src/components/CreateArtForm/CreateArtForm.stories.tsx
+++ libs/ui/src/components/CreateArtForm/CreateArtForm.stories.tsx
@@ -1,14 +1,7 @@
-import { useRef, useState } from 'react'
-
-import { useDisclosure } from '@chakra-ui/react'
-import slugify from '@sindresorhus/slugify'
-import { Story, Meta } from '@storybook/react'
-import { CATEGORY_MOCKS } from '@wsvvrijheid/mocks'
-import { Art, ArtCreateInput } from '@wsvvrijheid/types'
-import { createMutation } from '@wsvvrijheid/utils'
+import { Meta, Story } from '@storybook/react'
 
 import { CreateArtForm } from '.'
-import { CreateArtFormProps, CreateArtFormFieldValues } from './types'
+import { CreateArtFormProps } from './types'
 
 export default {
   title: 'Forms/CreateArtForm',
@@ -16,46 +9,30 @@ export default {
 } as Meta<CreateArtFormProps>
 
 const Template: Story<CreateArtFormProps> = args => {
-  const [isLoading, setIsLoading] = useState(false)
-  const createArtCancelRef = useRef<HTMLButtonElement>(null)
-  const createArtFormDisclosure = useDisclosure()
-
-  const handleCreateArt = async (
-    data: CreateArtFormFieldValues & { images: Blob[] },
-  ) => {
-    const slug = slugify(data.title)
-    const formBody = {
-      ...data,
-      slug,
-      artist: 1,
-      categories: data.categories.map(c => Number(c.value)),
-    }
-
-    await createMutation<Art, ArtCreateInput>('api/arts', formBody)
-
-    setIsLoading(true)
-    setTimeout(() => {
-      alert(JSON.stringify(data))
-      setIsLoading(false)
-    }, 2000)
-  }
-
-  return (
-    <CreateArtForm
-      {...args}
-      onCreateArt={handleCreateArt}
-      isLoading={isLoading}
-      categories={CATEGORY_MOCKS.data}
-      cancelRef={createArtCancelRef}
-      formDisclosure={createArtFormDisclosure}
-    />
-  )
+  return <CreateArtForm {...args} />
 }
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  auth: {
+    user: null,
+    isLoggedIn: false,
+    token: null,
+  },
+}
 
 export const Auth = Template.bind({})
 Auth.args = {
-  isLoggedIn: true,
+  auth: {
+    user: {
+      id: 38,
+      username: 'Artist',
+      email: 'artist@samenvvv.nl',
+      provider: 'local',
+      confirmed: true,
+      isAuthenticated: true,
+    },
+    isLoggedIn: true,
+    token: 'fake-token',
+  },
 }
