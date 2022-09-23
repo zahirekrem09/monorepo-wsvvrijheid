e3295266efd087caa94370b0b5a6d740cadc40e2
MSKose <saitmustafa1998@gmail.com>
2022-09-23 17:27:00 +0200

    1 and 2 added

--- libs/ui/src/examples/Fetch/E1_FetchAxios.tsx
+++ libs/ui/src/examples/Fetch/E1_FetchAxios.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 import { Box, Stack } from '@chakra-ui/react'
 import { API_URL, TOKEN } from '@wsvvrijheid/config'
+import axios from 'axios'
 
 const BLOG_URL = `${API_URL}/api/blogs` // https://api.samenvvv.nl/api/blogs
 const API_TOKEN = TOKEN
@@ -10,9 +11,25 @@ const API_TOKEN = TOKEN
 export const FetcAxios = () => {
   const [blogs, setBlogs] = useState([])
 
+  const getBlogs = async () => {
+    try {
+      const { data } = await axios.get(BLOG_URL);
+      setBlogs(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   useEffect(() => {
     // TODO: fetch blogs with axios by using the API_URL and TOKEN
+    getBlogs()
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  
+
+  return (<Box>
+    {/* TODO: Show only title of the blogs */}
+    {blogs.map(x => <Box>{x["title"]}</Box>)}
+    </Box>)
 }
--- libs/ui/src/examples/Fetch/E2_FetchFetcher.tsx
+++ libs/ui/src/examples/Fetch/E2_FetchFetcher.tsx
@@ -4,14 +4,34 @@ import { useEffect, useState } from 'react'
 import { Box, Stack } from '@chakra-ui/react'
 import { API_URL, TOKEN } from '@wsvvrijheid/config'
 import { fetcher } from '@wsvvrijheid/utils'
+import axios from 'axios'
+
+
+const BLOG_URL = `${API_URL}/api/blogs`
+const API_TOKEN = TOKEN
 
 export const FetchFetcher = () => {
   const [blogs, setBlogs] = useState([])
 
+  const getBlogs = async () => {
+    try {
+      const { data } = await fetcher(API_TOKEN)(BLOG_URL);
+      setBlogs(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     // TODO: fetch blogs with fetcher by using the API_URL and TOKEN
     // Remember that fetcher is a wrapper around axios that adds the token and api url to the request
+    getBlogs()
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  console.log(blogs);
+
+  return <Box>
+    {/* TODO: Show only title of the blogs */}
+    {blogs.map(x => <Box>{x["title"]}</Box>)}
+    </Box>
 }
