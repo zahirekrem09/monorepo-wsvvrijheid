f2040d5d49383aa9207950e1a23b8b545e43a67a
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2022-10-01 15:13:13 +0200

    ArtAddToCollectionCard.tsx formatted

--- libs/ui/src/admin/ArtAddToCollectionCard/ArtAddToCollectionCard.tsx
+++ libs/ui/src/admin/ArtAddToCollectionCard/ArtAddToCollectionCard.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from 'react'
 
 import { Box, Flex, Spacer, Stack, Text, useDisclosure } from '@chakra-ui/react'
 
-import {  ArtModal, WImage } from '../../components'
+import { ArtModal, WImage } from '../../components'
 import { ArtAddToCollectionCardProps } from './types'
 import { MutationButton } from '@wsvvrijheid/ui'
 import { AiOutlineEye } from 'react-icons/ai'
@@ -12,11 +12,12 @@ import { HiPlus } from 'react-icons/hi'
 export const ArtAddToCollectionCard: FC<ArtAddToCollectionCardProps> = ({
   isAdded,
   art,
-  onAdd, onRemove
+  onAdd,
+  onRemove,
 }) => {
-  const {isOpen, onOpen, onClose}= useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [added, setAdded] = useState(isAdded)
-  
+
   return (
     <Stack
       w={'300px'}
@@ -26,12 +27,7 @@ export const ArtAddToCollectionCard: FC<ArtAddToCollectionCardProps> = ({
       direction={'column'}
       overflow="hidden"
     >
-      <WImage
-        h={'200px'}
-        src={
-          art.images?.[0].url || ''
-        }
-      />
+      <WImage h={'200px'} src={art.images?.[0].url || ''} />
       <Stack pl={2} flex={1} justify="space-between">
         <Text fontSize="md" fontWeight="semibold" noOfLines={1}>
           {art.title}
@@ -41,32 +37,40 @@ export const ArtAddToCollectionCard: FC<ArtAddToCollectionCardProps> = ({
           {art.artist?.username}
         </Text>
       </Stack>
-      <Flex gap={'2'} w='full'>
-        <Box p='2'>
-        <MutationButton
-          icon={<AiOutlineEye />}
-          title="View"
-          onClick={onOpen}
-          variant={'ghost'}
-          colorScheme={'gray'}
-        />
+      <Flex gap={'2'} w="full">
+        <Box p="2">
+          <MutationButton
+            icon={<AiOutlineEye />}
+            title="View"
+            onClick={onOpen}
+            variant={'ghost'}
+            colorScheme={'gray'}
+          />
         </Box>
         <Spacer />
-        <Box p='2'>
-        <MutationButton
-          variant={'outline'}
-          colorScheme={added ? 'red' : 'green'}
-          icon={added ? <IoCloseSharp /> : <HiPlus />}
-          title={added ? 'Remove' : 'Add to Collection'}
-          onClick={() => {added ? onRemove() : onAdd(); setAdded(prev => !prev)}}
-        />
+        <Box p="2">
+          <MutationButton
+            variant={'outline'}
+            colorScheme={added ? 'red' : 'green'}
+            icon={added ? <IoCloseSharp /> : <HiPlus />}
+            title={added ? 'Remove' : 'Add to Collection'}
+            onClick={() => {
+              added ? onRemove() : onAdd()
+              setAdded(prev => !prev)
+            }}
+          />
         </Box>
       </Flex>
-      <ArtModal auth={{
-        user: null,
-        isLoggedIn: false,
-        token: null
-      }} art={art} isOpen={isOpen} onClose={onClose } />
+      <ArtModal
+        auth={{
+          user: null,
+          isLoggedIn: false,
+          token: null,
+        }}
+        art={art}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </Stack>
   )
 }
