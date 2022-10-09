23beb236085f296edc39d566a769a82092009b18
MSKose <saitmustafa1998@gmail.com>
2022-10-09 09:38:24 +0200

    E6_RequestuseQuery added

--- libs/ui/src/examples/Fetch/E6_RequestUseQuery.tsx
+++ libs/ui/src/examples/Fetch/E6_RequestUseQuery.tsx
@@ -5,17 +5,40 @@ import { Box, Stack } from '@chakra-ui/react'
 import { API_URL, TOKEN } from '@wsvvrijheid/config'
 import { Blog } from '@wsvvrijheid/types'
 import { request } from '@wsvvrijheid/utils'
+import axios from 'axios'
+import { useQuery, QueryKey } from '@tanstack/react-query'
+
+const BLOG_URL = `${API_URL}/api/blogs` // https://api.samenvvv.nl/api/blogs
+const API_TOKEN = TOKEN
 
 export type RequestUseQueryProps = {
   initialName: string
 }
 
+
 export const RequestUseQuery: FC<RequestUseQueryProps> = ({ initialName }) => {
-  const [blogs, setBlogs] = useState<Blog[]>([])
-  const [nameFilter, setNameFilter] = useState(initialName)
 
-  //   useEffect(() => {}, [])
+  const instance = axios.create({
+    headers: {'Authorization': 'Bearer ' + TOKEN}
+  });
+  
+   const getBlogs = async () => {
+    return await instance.get(BLOG_URL)
+  }
 
+  const { data, status } = useQuery(["title"], getBlogs);
   // TODO Add Input to change nameFilter
-  return <div>Hi</div>
+  return (
+    <div className="App">
+      {status === "error" && <p>Error fetching data</p>}
+      {status === "loading" && <p>Fetching data...</p>}
+      {status === "success" && (
+        <div>
+          {data.data.data.map((user: any) => (
+            <p key={user.id}>{user['title']} </p>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
